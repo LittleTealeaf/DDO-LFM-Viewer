4b851c0668cffa5368a9b964ceb13bf8b55bb747
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2020-07-03 20:38:53 -0400

    Spaced out requests over 15s
    Fetching Reports will only ping the API if 15 seconds has passed since the last ping, else it will use the report retrieved from the latest request

--- src/main/AuditAPI.java
+++ src/main/AuditAPI.java
@@ -6,10 +6,20 @@ import classes.Server;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.time.Instant;
+import java.util.Date;
 
+/**
+ * API integration with the <a href="https://www.playeraudit.com/">www.playeraudit.com</a> site, used with permission
+ * @author Tealeaf
+ * @see <a href="https://www.playeraudit.com/api/">www.playeraudit.com/api</a>
+ */
 public class AuditAPI {
 
     private static final String api = "https://www.playeraudit.com/api/groups";
+
+    private static String JSON;
+    private static Date lastRequest;
 
     public static Report fetchReport() {
         try {
@@ -21,9 +31,18 @@ public class AuditAPI {
     }
 
     private static String getJson() {
-        try {
-            return new BufferedReader(new InputStreamReader(new URL(api).openConnection().getInputStream())).readLine();
-        } catch(Exception e) {}
-        return null;
+        if(lastRequest == null || lastRequest.before(Date.from(Instant.now().minusSeconds(15)))) {
+            try {
+                lastRequest = Date.from(Instant.now());
+                System.out.println("Requested JSON at " + lastRequest.toString());
+                return JSON = new BufferedReader(new InputStreamReader(new URL(api).openConnection().getInputStream())).readLine();
+            } catch(Exception e) {
+                return JSON;
+            }
+        } else {
+            return JSON;
+        }
+
+
     }
 }
